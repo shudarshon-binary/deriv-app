d7059936d62817523373e05264548f5c434802f0
negar <negar@binary.com>
2019-10-02 15:41:22 +0800

    Updates

---
+++ bot/js/bot/36-5b7765.bot.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[36],{613:function(t){t.exports=JSON.parse('{"text":["This blocks transfers control to the Purchase conditions block. This is block id not mandatory, however you most probably want to use it in you strategy as it\'s the only way to purchase another contract without manually stopping and restarting you strategy.","Example:"]}')}}]);
+//# sourceMappingURL=36-5b7765.bot.js.map
