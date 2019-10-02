d1808dbb76f0798b5e8316ca10243e5f702badf9
negar <negar@binary.com>
2019-10-02 17:37:00 +0800

    Updates

--- bot/js/bot/28-beb535.bot.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[28],{605:function(t){t.exports=JSON.parse('{"text":["This block sells your active contract at market price. You don\'t necessarily need to sell your contract, however you might want to do it if the market trend is against you.","Example:"]}')}}]);
-//# sourceMappingURL=28-beb535.bot.js.map
