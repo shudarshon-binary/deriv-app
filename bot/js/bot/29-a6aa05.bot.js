d68ffe86e83af0eeb23e7a4622d5bc54ef265568
mahboobehmohammadi <mahboub.meng@gmail.com>
2019-10-03 12:50:40 +0800

    Updates

--- bot/js/bot/29-a6aa05.bot.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[29],{612:function(e){e.exports=JSON.parse('{"text":["This block can be used only within “Sell conditions” root block.","The example below sells active contract in case if Sell profit/loss is greater than the stake assuming that variable \\"my_stake\\" was defined before."]}')}}]);
-//# sourceMappingURL=29-a6aa05.bot.js.map
--- bot/js/bot/31-67f77a.bot.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonpderiv_bot=window.webpackJsonpderiv_bot||[]).push([[31],{614:function(e){e.exports=JSON.parse('{"text":["This block displays a modal window with a given message.","Strategy execution is being paused until user clicks \'OK\'"]}')}}]);
-//# sourceMappingURL=31-67f77a.bot.js.map
